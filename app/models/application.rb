ebe5fa3dccbd397c7ac3c9d91bdfc86fcb0b6153
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-29 22:11:43 -0700

    finishing touches

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,7 +1,7 @@
 class Application < ApplicationRecord
-  has_many :companies
+  has_many :companies, dependent: :destroy
   has_many :users, :through => :companies
-  
+
   validates :company, :date, presence: true
   validates_inclusion_of :followup, in: [true, false]
 end
