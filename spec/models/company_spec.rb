21eec22365c83a422e6332c1d564d23aa3424f6b
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-29 19:42:45 -0700

    finish user validation tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,9 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  it 'is valid with valid attributes'
-  it 'is invalid without a name'
-  it 'is invalid without an address'
-  it 'is invalid without a telephone number'
-  it 'is invalid without a contact person'
+  it 'is valid with valid attributes' do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
+    company = Company.create(name: "Codeco",
+                             address: "123 Avenue St",
+                             telephone_number: "503-555-5555",
+                             contact_person: "Steve Jobs",
+                             user_id: user.id)
+
+    expect(company).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
+    company = Company.create(name: "",
+                             address: "123 Avenue St",
+                             telephone_number: "503-555-5555",
+                             contact_person: "Steve Jobs",
+                             user_id: user.id)
+
+    expect(company).to be_invalid
+  end
+
+  it 'is invalid without an address' do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
+    company = Company.create(name: "Codeco",
+                             address: "",
+                             telephone_number: "503-555-5555",
+                             contact_person: "Steve Jobs",
+                             user_id: user.id)
+
+    expect(company).to be_invalid
+  end
+
+  it 'is invalid without a telephone number' do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
+    company = Company.create(name: "Codeco",
+                             address: "123 Avenue St",
+                             telephone_number: "",
+                             contact_person: "Steve Jobs",
+                             user_id: user.id)
+
+     expect(company).to be_invalid
+   end
+
+  it 'is invalid without a contact person' do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
+    company = Company.create(name: "Codeco",
+                             address: "123 Avenue St",
+                             telephone_number: "503-555-5555",
+                             contact_person: "",
+                             user_id: user.id)
+
+     expect(company).to be_invalid
+  end
 end
