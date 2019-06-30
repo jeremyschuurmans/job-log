461999b3be331248a51ddc302d290ade9b31aba4
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-29 19:09:23 -0700

    add first test, has_secure_password, validations in user model, bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-
+  has_secure_password
+  validates :name, :email, :password_digest, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it 'is valid if it has valid attributes'
+  it 'is valid if it has valid attributes' do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password_digest: "securepassword")
+
+    expect(user).to be_valid
+  end
 
 
   it 'is invalid without a name'
