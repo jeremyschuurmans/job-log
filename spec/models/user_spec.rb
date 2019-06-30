fcb2f3cb37afb277d0fc47bfbc93d892c8c63530
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-29 19:14:49 -0700

    finish testing user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,14 +4,37 @@ RSpec.describe User, :type => :model do
   it 'is valid if it has valid attributes' do
     user = User.create(name: "Grace Hopper",
                        email: "admiralgrace@googlemail.com",
-                       password_digest: "securepassword")
+                       password: "securepassword")
 
     expect(user).to be_valid
+    user.delete
   end
 
+  it 'is invalid without a name' do
+    user = User.create(name: "",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
 
-  it 'is invalid without a name'
-  it 'is invalid without an email'
-  it 'is invalid without a password'
+    expect(user).to be_invalid
+    user.delete
+  end
+
+  it 'is invalid without an email' do
+    user = User.create(name: "Grace Hopper",
+                       email: "",
+                       password: "secure_password")
+
+    expect(user).to be_invalid
+    user.delete
+  end
+
+  it 'is invalid without a password' do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "")
+
+    expect(user).to be_invalid
+    user.delete
+  end
 
 end
