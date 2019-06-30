85dc6e695e45e68c9d44c13fbe4130cf648ac438
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-29 18:29:24 -0700

    stub out test for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, :type => :model do
+  it 'is valid if it has valid attributes'
+
+
+  it 'is invalid without a name'
+  it 'is invalid without an email'
+  it 'is invalid without a password'
+
+end
