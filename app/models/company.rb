f7255a9a7078e4a808dd5da42a2e8ce357453ef1
Jeremy Schuurmans <schuurm@ns.codes>
2019-06-30 21:32:29 -0700

    finish user association tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,7 @@
 class Company < ApplicationRecord
   belongs_to :user
   belongs_to :application
-  
+
   validates :name, :address, :telephone_number, :contact_person, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  it 'is valid if it has valid attributes' do
-    user = User.create(name: "Grace Hopper",
-                       email: "admiralgrace@googlemail.com",
-                       password: "securepassword")
 
-    expect(user).to be_valid
-    user.delete
+  describe 'validations' do
+    it 'is valid if it has valid attributes' do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "securepassword")
+
+      expect(user).to be_valid
+      user.delete
+    end
+
+    it 'is invalid without a name' do
+      user = User.create(name: "",
+                         email: "admiralgrace@googlemail.com",
+                         password: "securepassword")
+
+      expect(user).to be_invalid
+      user.delete
+    end
+
+    it 'is invalid without an email' do
+      user = User.create(name: "Grace Hopper",
+                         email: "",
+                         password: "secure_password")
+
+      expect(user).to be_invalid
+      user.delete
+    end
+
+    it 'is invalid without a password' do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "")
+
+      expect(user).to be_invalid
+      user.delete
+    end
   end
 
-  it 'is invalid without a name' do
-    user = User.create(name: "",
-                       email: "admiralgrace@googlemail.com",
-                       password: "securepassword")
+  describe 'associations' do
+    it 'has many companies' do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "password")
 
-    expect(user).to be_invalid
-    user.delete
+      application_1 = Application.create(company: "Codeco", date: 06/29/2019, followup: false)
+
+      application_2 = Application.create(company: "Braeburn", date: 06/29/2019, followup: false)
+
+
+      company_1 = Company.create(name: "Codeco",
+                                 address: "123 Avenue St",
+                                 telephone_number: "503-555-5555",
+                                 contact_person: "Steve Jobs",
+                                 user_id: user.id,
+                                 application_id: application_1.id)
+
+      company_2 = Company.create(name: "Braeburn",
+                                 address: "123 Apple St",
+                                 telephone_number: "503-555-5555",
+                                 contact_person: "Steve Gates",
+                                 user_id: user.id,
+                                 application_id: application_2.id)
+
+      expect(user.companies.first).to eq(company_1)
+      expect(user.companies.last).to eq(company_2)
+    end
+
+    it 'has many applications through companies' do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "password")
+
+      application_1 = Application.create(company: "Codeco", date: 06/29/2019, followup: false)
+
+      application_2 = Application.create(company: "Braeburn", date: 06/29/2019, followup: false)
+
+
+      company_1 = Company.create(name: "Codeco",
+                                 address: "123 Avenue St",
+                                 telephone_number: "503-555-5555",
+                                 contact_person: "Steve Jobs",
+                                 user_id: user.id,
+                                 application_id: application_1.id)
+
+      company_2 = Company.create(name: "Braeburn",
+                                 address: "123 Apple St",
+                                 telephone_number: "503-555-5555",
+                                 contact_person: "Steve Gates",
+                                 user_id: user.id,
+                                 application_id: application_2.id)
+
+      expect(user.applications.first).to eq(application_1)
+      expect(user.applications.last).to eq(application_2)
+    end
   end
-
-  it 'is invalid without an email' do
-    user = User.create(name: "Grace Hopper",
-                       email: "",
-                       password: "secure_password")
-
-    expect(user).to be_invalid
-    user.delete
-  end
-
-  it 'is invalid without a password' do
-    user = User.create(name: "Grace Hopper",
-                       email: "admiralgrace@googlemail.com",
-                       password: "")
-
-    expect(user).to be_invalid
-    user.delete
-  end
-
 end
