6fbeba05583d5061a88c6a1d761f8116c9b56c7c
Jeremy Schuurmans <schuurm@ns.codes>
2019-07-10 15:31:23 -0700

    add some tests for users controller and sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  render_views
 
+  describe "Log in page" do
+    it 'loads the log in page' do
+      get :new
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Log In")
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  render_views
 
+  describe "Sign Up Page" do
+    it 'loads the sign up page' do
+      get :new
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Sign Up")
+    end
+
+    it 'redirects user to companies#index' do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "securepassword")
+
+      expect(user).to be_a_new(User)
+    end
+  end
 end
