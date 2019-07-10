25866a2cab0913f5df98b8bd5e2a9509e50fe947
Jeremy Schuurmans <schuurm@ns.codes>
2019-07-10 14:48:51 -0700

    create route for homepage, tests, and basic content

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
 
+  describe "Homepage" do
+    it 'loads the homepage' do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Welcome to JobLog")
+    end
+
+    it 'contains a sign up link' do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Sign Up")
+    end
+
+    it 'contains a log in link' do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Log In")
+    end
+  end
 end
