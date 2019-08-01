1c4ea47b2e4e31dce584bb44ec004207b54c084f
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-01 12:42:25 -0700

    update users_controller#create test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -18,12 +18,14 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "Create" do
-    it 'redirects user to companies#index upon creation' do
+    it 'persists a user into the database' do
       user = User.create(name: "Grace Hopper",
                          email: "admiralgrace@googlemail.com",
                          password: "securepassword")
 
       expect(user).to_not be_a_new(User)
+      expect(user.id).to eq(1)
+      expect(user.id).to_not eq(2)
     end
   end
 end
