f18871fe85683d39d5f709ea0c87277411ad0b42
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-01 15:41:06 -0700

    add test for invalid login

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "UserLogIns", type: :feature do
 
-  feature 'User Logs In' do
+  feature 'User logs in' do
     scenario 'with valid email and password' do
       user = User.create(name: "Grace Hopper",
                          email: "admiralgrace@googlemail.com",
@@ -15,6 +15,20 @@ RSpec.feature "UserLogIns", type: :feature do
 
       expect(current_path).to eq('/companies')
       expect(page).to have_content('Sign Out')
+    end
+
+    scenario 'with invalid email or password' do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "securepassword")
+
+      visit login_path
+      fill_in 'session_email', with: 'bob@bob.com'
+      fill_in 'session_password', with: 'bobspassword'
+      click_button 'Log In'
+
+      expect(current_path).to eq('/login')
+      expect(page).to have_content('Log In')
     end
   end
 
