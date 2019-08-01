fb805b4b98df960e069d9c2c430f56b573bf717f
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-01 13:48:43 -0700

    add test for same user not being able to sign up twice and uniqueness validation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :name, :email, :password_digest, presence: true
+  validates :name, :email, :password_digest, presence: true, uniqueness: true
 end
--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -23,4 +23,25 @@ RSpec.feature "UserSignUps", type: :feature do
       expect(page).to have_content('Sign Up')
     end
   end
+
+  feature 'Same User Cannot Sign Up Twice' do
+    scenario 'with valid name, email, and password' do
+      visit signup_path
+      fill_in 'user_name', with: 'Ada Lovelace'
+      fill_in 'user_email', with: 'ada@googlemail.com'
+      fill_in 'user_password', with: 'password'
+      click_button 'Sign Up'
+
+      expect(current_path).to eq('/companies')
+      expect(page).to have_content('Sign Out')
+
+      visit signup_path
+      fill_in 'user_name', with: 'Ada Lovelace'
+      fill_in 'user_email', with: 'ada@googlemail.com'
+      fill_in 'user_password', with: 'password'
+      click_button 'Sign Up'
+      expect(current_path).to eq('/signup')
+      expect(page).to have_content('Sign Up')
+    end
+  end
 end
