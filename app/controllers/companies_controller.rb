35902156ad069845e5ff2a81e4da533d1d0372ff
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-02 19:49:22 -0700

    create action and company create form works. Needs refactoring

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,4 +7,21 @@ class CompaniesController < ApplicationController
     @company = Company.new
     @user = User.find_by(params[:id])
   end
+
+  def create
+    @user = User.find_by(params[:id]) #change to current_user and create current_user method when this works
+    @company = @user.companies.build(company_params)
+    if @company.save
+      @user.companies << @company
+      redirect_to companies_url
+    else
+      redirect_to new_company_url
+    end
+  end
+
+  private
+
+    def company_params
+      params.require(:company).permit(:name, :address, :telephone_number, :contact_person, :user_id)
+    end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   belongs_to :user
-  belongs_to :application
+  belongs_to :application, optional: true
 
   validates :name, :address, :telephone_number, :contact_person, presence: true
 
