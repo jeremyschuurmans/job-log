554f217e383babce901b965a078f95fd73a6e6f0
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-02 18:07:35 -0700

    add test for companies index page

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
+  render_views
+
+  describe "Index" do
+    it "loads the companies index page" do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Companies")
+    end
+  end
 
 end
