eb7f76e2b9b59dbb17a9459750a7bbd1228485af
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-05 11:48:24 -0700

    create applications_controller, add specs

---
+++ spec/controllers/applications_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationsController, type: :controller do
+  render_views
+
+  describe "Index" do
+    it "loads the applications index page" do
+      get :index
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Applications")
+    end
+
+    it "displays all applications" do
+    end
+  end
+
+  describe "New" do
+    it "loads a new application form" do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "securepassword")
+      get 'applications/new'
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Submit Application")
+    end
+
+    it "instantiates a new application" do
+      application = Application.new
+
+      expect(application).to be_a_new(Application)
+    end
+  end
+end
