1065068e4feddeb13c8580253fe21914f7e1166d
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-06 14:40:49 -0700

    realize I only want log in checks for the edit and update actions in the users controller that I haven't written yet. Add them.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :check_if_user_logged_in
+  before_action :check_if_user_logged_in, only: [:edit, :update]
   before_action :is_this_the_right_user?, only: [:edit, :update]
 
   def new
