bd3a974bb6b5918173bf399007d939882d3833e2
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-06 19:22:19 -0700

    improve validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   has_many :applications, dependent: :destroy
   has_many :companies, :through => :applications
-
+  before_save {self.email = email.downcase}
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true,
+                    length: { maximum: 100 },
+                    format: { with: URI::MailTo::EMAIL_REGEXP },
+                    uniqueness: true, { case_sensitive: false }
   has_secure_password
-
-  validates :name, :email, :password_digest, presence: true, uniqueness: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
 end
