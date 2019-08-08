a2f9399c12c26482496752451f19fe6e7eba24fc
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-08 13:13:42 -0700

    add method for turning company names into companyname.com format, add company logos to company show page

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,7 @@ class Company < ApplicationRecord
 
   validates :name, :address, :telephone_number, :contact_person, presence: true
 
+  def company_url
+    self.name.gsub(" ", "").downcase.concat('.com')
+  end
 end
