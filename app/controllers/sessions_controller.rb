0f4ae1a0bcdab2c678e7921fb809f4cb1bf78677
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-09 11:17:39 -0700

    add auth helper method and login_or_create_user_via_omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      redirect_back_or companies_url
+    if auth
+      login_or_create_user_via_omniauth
     else
-      flash[:alert] = "Username or password is invalid."
-      redirect_to login_url
+      @user = User.find_by(email: params[:session][:email])
+      if @user && @user.authenticate(params[:session][:password])
+        log_in @user
+        redirect_back_or companies_url
+      else
+        flash[:alert] = "Username or password is invalid."
+        redirect_to login_url
+      end
     end
   end
 
@@ -19,4 +23,23 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_url
   end
+
+  private
+
+    def auth
+      Rails.application.env_config['omniauth.auth']
+    end
+
+    def login_or_create_user_via_omniauth
+      @user = User.find_by(email: auth['info']['email'])
+      if @user
+        log_in @user
+        redirect_to companies_url
+      else
+        password = SecureRandom.urlsafe_base64
+        @user = User.create(name: auth['info']['name'], email: auth['info']['email'], password: password)
+        log_in @user
+        redirect_to companies_url
+      end
+    end
 end
