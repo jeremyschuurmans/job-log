fa3fcf0d67c145d22b57028fb9e1f3027c950a3f
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-09 10:30:54 -0700

    add specs for normal log in and log out

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,10 +4,31 @@ RSpec.describe SessionsController, type: :controller do
   render_views
 
   describe "Log in page" do
-    it 'loads the log in page' do
+    it "loads the log in page" do
       get :new
       expect(response.status).to eq(200)
       expect(response.body).to include("Log In")
     end
+  end
+
+  it "can create a new session" do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
+
+    post :create, params: { session: { email: "admiralgrace@googlemail.com", password: "securepassword" } }
+    expect(session[:user_id]).to eq(user.id)
+  end
+
+  it "can delete a session" do
+    user = User.create(name: "Grace Hopper",
+                       email: "admiralgrace@googlemail.com",
+                       password: "securepassword")
+
+    post :create, params: { session: { email: "admiralgrace@googlemail.com", password: "securepassword" } }
+    expect(session[:user_id]).to eq(user.id)
+
+    delete :destroy, params: { session: { user_id: user.id } }
+    expect(session[:user_id]).to eq(nil)
   end
 end
