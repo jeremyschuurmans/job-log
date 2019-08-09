44bd8c439f181d0900c873919e6835ca610096b9
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-08 22:25:07 -0700

    edit tests

--- spec/features/application_create_spec.rb
+++ spec/features/application_create_spec.rb
@@ -33,9 +33,9 @@ RSpec.feature "CreateApplications", type: :feature do
       expect(Application.last.company_id).to eq(Company.last.id)
       expect(Application.last.company_name).to eq(nil)
 
-      expect(current_path).to eq('/companies/3')
+      expect(current_path).to eq('/companies/7')
       click_link 'Add a New Application'
-      expect(current_path).to eq('/companies/3/applications/new')
+      expect(current_path).to eq('/companies/7/applications/new')
       expect(page).to have_content("Please select the company you are applying to:")
       find('#application_company_name').find(:xpath, 'option[2]').select_option
       fill_in 'application_date', with: '2019/10/22'
@@ -44,7 +44,7 @@ RSpec.feature "CreateApplications", type: :feature do
       expect(user.applications.last.company_name).to eq("Planet Argon")
       expect(Company.last.applications.last.company_name).to eq("Planet Argon")
       expect(User.last.applications.count).to eq(1)
-      expect(current_path).to eq('/companies/3/applications/1')
+      expect(current_path).to eq('/companies/7/applications/5')
       expect(page).to have_content('Planet Argon')
     end
   end
