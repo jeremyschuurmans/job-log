004cae2a8a054645b1563913cfce1233aed46fb5
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-09 09:25:05 -0700

    edit companies_controller_spec, add create action for applications_controller_spec

--- spec/controllers/applications_controller_spec.rb
+++ spec/controllers/applications_controller_spec.rb
@@ -49,4 +49,29 @@ RSpec.describe ApplicationsController, type: :controller do
       expect(application).to be_a_new(Application)
     end
   end
+
+  describe "Create" do
+    it "populates application datafields in database" do
+      user = User.create(name: "Grace Hopper",
+                         email: "admiralgrace@googlemail.com",
+                         password: "securepassword")
+      company = user.companies.create(name: "Pied Piper",
+                                      address: { :street_address =>  "12341234 Recursion Way",
+                                                 :city => "Portland",
+                                                 :state => "Oregon",
+                                                 :zip_code => 97224 },
+                                      telephone_number: 555-555-5555,
+                                      contact_person: "Nelson Bighetti")
+
+      expect(company.name).to eq("Pied Piper")
+      expect(company.id).to eq(3)
+      expect(Application.last.company_id).to eq(company.id)
+      expect(Application.last.company_name).to eq(nil)
+
+      application = Application.find_by(company_id: company.id)
+      application.update(company_name: company.name, date: "2019-10-22")
+      expect(Application.last.company_id).to eq(company.id)
+      expect(Application.last.company_name).to eq(company.name)
+    end
+  end
 end
