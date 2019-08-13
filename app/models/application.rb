383f7c9df874fb41b64e05e0d07f6ae3a4fe31d4
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-12 21:43:16 -0700

    conditionally render followup response form and response

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,12 @@ class Application < ApplicationRecord
 
   validates :company_name, :date, presence: true, allow_nil: true
   validates_inclusion_of :followup, in: [true, false]
+
+  def followed_up
+    "Followed up"
+  end
+
+  def not_followed_up
+    "Not yet followed up"
+  end
 end
