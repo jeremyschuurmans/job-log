2a1c8e68a4c30a00fe2d6f97147a0644aa1f01b7
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-15 20:13:10 -0700

    style navbar, minor edits, move javascript to .js file

---
+++ app/assets/javascripts/companies.js
@@ -0,0 +1,33 @@
+$(function() {
+  $(".show-all").on("click", function() {
+    $.get("/applications" + ".json", function(data) {
+      let companies = data.map(c => {
+        if (c.date !== null) {
+        return `
+          <div class="company-info">
+            <ul>
+              <li>
+                <a href="/companies/${c.company.id}/applications/${c.id}">${c.company_name}</a>
+                <p>${c.date}</p>
+              </li>
+            </ul>
+          </div>`
+        } else {
+          return `
+            <div class="company-info">
+              <ul>
+                <li>
+                  <a href="/companies/${c.company.id}/applications/${c.id}">${c.company_name}</a>
+                  <p>Not yet applied</p>
+                </li>
+              </ul>
+            </div>`
+          };
+        }).join(' ');
+      $("#apps").html(companies);
+    });
+    $(".clear-all").on("click", function() {
+      $(".company-info").hide();
+    });
+  });
+});
