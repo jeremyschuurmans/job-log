2b9e696e720d7bc02feb4241c61e250f5f1de0cb
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-16 18:41:44 -0700

    fix page-title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 
   def full_title(page_title = "")
     base_title = "JobLog"
-    page_title.empty? ? base_title : page_title + "-" + base_title
+    page_title.empty? ? base_title : page_title + "|" + base_title
   end
 end
