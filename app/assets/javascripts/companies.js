37fdc743f69e0f324937bf5b4460fb5430938fa4
Jeremy Schuurmans <schuurm@ns.codes>
2019-08-16 17:40:32 -0700

    move js into assets folder, create company_card partial

--- app/assets/javascripts/companies.js
+++ app/assets/javascripts/companies.js
@@ -31,3 +31,27 @@ $(function() {
     });
   });
 });
+
+$(function() {
+  $(".next-company").on("click", function() {
+    let companyId = parseInt($(".next-company").attr("company-id")) + 1;
+    $.getJSON("/companies/" + companyId + ".json", function(data) {
+      let companyUrl = data.name.toLowerCase().replace(/\s/g, '') + ".com";
+      $(".company-logo").attr("src", "https://logo.clearbit.com/" + companyUrl)
+      $(".company-name").text(data.name);
+      $(".company-street-address").text(data.address.street_address);
+      $(".company-city").text(data.address.city);
+      $(".company-state").text(data.address.state);
+      $(".company-zip").text(data.address.zip);
+      $(".company-telephone").text(data.telephone_number);
+      $(".company-contact").text(data.contact_person);
+      if (data.applied === true) {
+        $(".company-applied").text("Applied");
+      } else {
+        $(".company-applied").text("Not yet applied");
+      };
+
+      $(".next-company").attr("company-id", data.id);
+    });
+  });
+});
